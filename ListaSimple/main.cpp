8ce0f5a345ea13673aa1047807747909a057dacb
Diego Domínguez <diego.dominguez0356@alumnos.udg.mx>
2022-09-10 15:37:27 -0500

    Creando menú principal para interactuar con la lista
    Aunque no esté terminado aún

--- ListaSimple/main.cpp
+++ ListaSimple/main.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 
 int main(){
+	int opc = 0 ;
 	cout << "Hola!" << endl;
+	while(opc != 10){
+		cout << "Lista simple" << endl;
+		cout << "1. Añadir al inicio" << endl;
+		cout << "2. Mostrar" << endl;
+		
+		cin >> opc;
+
+		switch(opc){
+			// Añadir al inicio
+			case 1:
+				int valor;
+				cout << "Qué valor quieres agregar?" << endl;
+				cin >> valor;
+			case 2:
+				cout << "Hola, cómo estás" << endl;
+
+		}
+	}
 	return 0;
 }
